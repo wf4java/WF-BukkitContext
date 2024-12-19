cc9c9f7ba539e3f6ad28007568bac5ee1071a87f
wf4java <momopopo659@gmail.com>
2024-12-19 16:21:33 +0300

    Update to 1.0.2

--- src/main/java/wf/bukkit/context/depeneds/config/model/AbstractConfig.java
+++ src/main/java/wf/bukkit/context/depeneds/config/model/AbstractConfig.java
@@ -1,9 +1,10 @@
 package wf.bukkit.context.depeneds.config.model;
 
 
+
 import ru.cwcode.tkach.config.jackson.yaml.YmlConfig;
 
-public abstract class AbstractConfig extends YmlConfig  {
+public abstract class AbstractConfig extends YmlConfig {
 
     public String[] header() {
         return new String[]{"Configuration file"};
